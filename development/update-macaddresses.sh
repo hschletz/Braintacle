812b7dfeb331df6b940537ece16d4aac094b2fbc
Holger Schletz <holger.schletz@web.de>
2012-01-04 16:23:45 +0000

    Fixed incorrect header.

--- development/update-macaddresses.sh
+++ development/update-macaddresses.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# Validate code formatting
+# Update MAC address database.
 #
 # $Id$
-# 
+#
 # Copyright (C) 2011,2012 Holger Schletz <holger.schletz@web.de>
-# 
+#
 # This program is free software; you can redistribute it and/or modify it
 # under the terms of the GNU General Public License as published by the Free
 # Software Foundation; either version 2 of the License, or (at your option)
