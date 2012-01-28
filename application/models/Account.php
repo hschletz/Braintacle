b87c7b173d594b3390cf5dd3cb1b5ee035acabcb
Holger Schletz <holger.schletz@web.de>
2012-01-28 11:39:44 +0000

    Implemented account creation.

--- application/models/Account.php
+++ application/models/Account.php
@@ -25,11 +25,44 @@
 /**
  * Class representing a Braintacle user account
  *
- * This is the interface to Braintacle's user accounts.
+ * This is the interface to Braintacle's user accounts. Available properties are:
+ *
+ * * Id: Login name
+ * * FirstName: First name (optional)
+ * * LastName: Last name (optional)
+ * * MailAddress: E-Mail address (optional)
+ * * Comment: Comment (optional)
+ *
+ * There is intentionally no property for the password. The password is
+ * write-only for security reasons and can only be set via {@link create()}.
+ *
+ * @method string getId() Retrieve login name
+ * @method string getFirstName() Retrieve first name
+ * @method string getLastName() Retrieve last name
+ * @method string getMailaddress() Retrieve E-Mail address
+ * @method string getComment() Retrieve comment
  * @package Models
  */
 class Model_Account extends Model_Abstract
 {
+
+    /**
+     * Constants for access privileges
+     *
+     * There are 2 equivalent columns that specify the privileges: 'accesslvl'
+     * (OLD_PRIVILEGE_* constants) and new_accesslvl (PRIVILEGE_* constants).
+     * This is a mess, and both columns should be always be used for maximum
+     * compatibility.
+     *
+     * Braintacle does not support different privileges. All accounts are
+     * created with admin privileges, and only admin accounts can log in. For
+     * this reason, constants are defined only for the ADMIN privilege.
+     */
+    const PRIVILEGE_ADMIN = 'sadmin';
+    /**
+     * See description for PRIVILEGE_ADMIN
+     */
+    const OLD_PRIVILEGE_ADMIN = 1;
 
     /** {@inheritdoc} */
     protected $_propertyMap = array(
@@ -62,4 +95,36 @@ class Model_Account extends Model_Abstract
             ->query();
     }
 
+    /**
+     * Create new account
+     * @param array $data List of properties to set. Unknown keys will be ignored.
+     * @param string $password Password for the new account, must not be empty.
+     * @throws UnexpectedValueException if no ID or password is given.
+     */
+    public static function create($data, $password)
+    {
+        if (!$data['Id']) {
+            throw new UnexpectedValueException('No login name specified');
+        }
+        if (!$password) {
+            throw new UnexpectedValueException('No password specified');
+        }
+
+        $dummy = new self;
+        $map = $dummy->getPropertyMap();
+
+        // Compose array of columns to set
+        foreach ($data as $property => $value) {
+            if (isset($map[$property])) { // Ignore unknown keys
+                $insert[$map[$property]] = $value;
+            }
+        }
+        // Set password
+        $insert['passwd'] = md5($password);
+        // Set admin privilege
+        $insert['accesslvl'] = self::OLD_PRIVILEGE_ADMIN;
+        $insert['new_accesslvl'] = self::PRIVILEGE_ADMIN;
+
+        Zend_Registry::get('db')->insert('operators', $insert);
+    }
 }
