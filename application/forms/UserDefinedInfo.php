368e8497f41759ffcf48c2c1c7640b4b7d26657a
Holger Schletz <holger.schletz@web.de>
2012-11-01 16:40:11 +0000

    Encode form element names.

--- application/forms/UserDefinedInfo.php
+++ application/forms/UserDefinedInfo.php
@@ -23,10 +23,6 @@
  * @package Forms
  */
 /**
- * Includes
- */
-require_once ('Braintacle/Validate/Date.php');
-/**
  * Form for display/setting of user defined fields for a computer.
  *
  * The field names are automatically retrieved from the database. Integer,
@@ -46,6 +42,12 @@ class Form_UserDefinedInfo extends Form_Normalized
     protected $_types;
 
     /**
+     * Instance of Braintacle_Filter_FormElementNameEncode, created by init()
+     * @var Braintacle_Filter_FormElementNameEncode
+     **/
+    protected $_encoder;
+
+    /**
      * Retrieve field name and types from the database and create form elements.
      */
     public function init()
@@ -53,23 +55,26 @@ class Form_UserDefinedInfo extends Form_Normalized
         $translate = Zend_Registry::get('Zend_Translate');
 
         $this->setMethod('post');
+        $this->_encoder = new Braintacle_Filter_FormElementNameEncode;
 
         // Category ('TAG') always comes first
-        $category = new Zend_Form_Element_Text('TAG');
+        $category = new Zend_Form_Element_Text($this->_encoder->filter('TAG'));
         $category->addFilter('StringTrim')
                  ->addValidator('StringLength', false, array(0, 255))
                  ->setLabel('Category');
         $this->addElement($category);
 
-        $this->_types = Model_UserDefinedInfo::getTypes();
-        foreach ($this->_types as $name => $type) {
+        foreach (Model_UserDefinedInfo::getTypes() as $name => $type) {
+            $elementName = $this->_encoder->filter($name);
+            $this->_types[$elementName] = $type;
+
             if ($name == 'TAG') {
                 continue; // element already created
             }
             if ($type == 'clob') {
-                $element = new Zend_Form_Element_Textarea($name);
+                $element = new Zend_Form_Element_Textarea($elementName);
             } else {
-                $element = new Zend_Form_Element_Text($name);
+                $element = new Zend_Form_Element_Text($elementName);
             }
             $element->setDisableTranslator(true) // Don't translate user-defined names
                     ->setLabel($name)
@@ -91,17 +96,51 @@ class Form_UserDefinedInfo extends Form_Normalized
             $this->addElement($element);
         }
 
-        $submit = new Zend_Form_Element_Submit('submit');
+        $submit = new Zend_Form_Element_Submit('_submit');
         $submit->setLabel('Change');
         $this->addElement($submit);
     }
 
     /**
      * Get the datatype for an element
+     * @param string $name Element name
      */
     public function getType($name)
     {
         return $this->_types[$name];
+    }
+
+    /** {@inheritdoc} */
+    public function getValue($name)
+    {
+        return parent::getValue($this->_encoder->filter($name));
+    }
+
+    /** {@inheritdoc} */
+    public function getValues($suppressArrayNotation = false)
+    {
+        $decoder = new Braintacle_Filter_FormElementNameDecode;
+        $values = array();
+        foreach (parent::getValues($suppressArrayNotation) as $name => $value) {
+            $values[$decoder->filter($name)] = $value;
+        }
+        return $values;
+    }
+
+    /** {@inheritdoc} */
+    public function setDefault($name, $value)
+    {
+        return parent::setDefault($this->_encoder->filter($name), $value);
+    }
+
+    /** {@inheritdoc} */
+    public function setDefaults(array $defaults)
+    {
+        $values = array();
+        foreach ($defaults as $name => $value) {
+            $values[$this->_encoder->filter($name)] = $value;
+        }
+        return parent::setDefaults($values);
     }
 
 }
