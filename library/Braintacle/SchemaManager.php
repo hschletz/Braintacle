hschletz/Braintacle

commit 6bfb1b016bc9f73823ea4bfb4f90eede96020c98

Support entering a Windows product key different from the inventoried one.

library/Braintacle/SchemaManager.php
@@ -47,7 +47,7 @@ class Braintacle_SchemaManager
     /**
      * Latest version of the database schema
      */
-    const SCHEMA_VERSION = 4;
+    const SCHEMA_VERSION = 5;
 
     /**
      * Database adapter

application/controllers/ComputerController.php
@@ -109,6 +109,22 @@ class ComputerController extends Zend_Controller_Action
 
     public function windowsAction()
     {
+        $windows = $this->computer->getWindows();
+
+        if (Model_Database::supportsManualProductKey()) {
+            $form = new Form_ProductKey;
+
+            if ($this->getRequest()->isPost() and $form->isValid($_POST)) {
+                $windows->setManualProductKey($form->key->getValue());
+            }
+
+            // Always retrieve key, even if it has just been set, because the
+            // model may have altered it.
+            $form->key->setValue($windows->getManualProductKey());
+            $this->view->form = $form;
+        }
+
+        $this->view->windows = $windows;
     }
 
     public function networkAction()

application/forms/ProductKey.php
@@ -1,0 +1,55 @@
+<?php
+/**
+ * A form for entering an MS product key
+ *
+ * $Id$
+ *
+ * Copyright (C) 2011,2012 Holger Schletz <holger.schletz@web.de>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * @package Forms
+ */
+/**
+ * Includes
+ */
+require_once ('Braintacle/Validate/ProductKey.php');
+/**
+ * A form for entering an MS product key
+ *
+ * The product key is held in the 'key' element.
+ * @package Forms
+ */
+class Form_ProductKey extends Zend_Form
+{
+
+    /**
+     * Create elements
+     */
+    public function init()
+    {
+        $key = new Zend_Form_Element_Text('key');
+        $key->setLabel('Product key (if different)')
+            ->addFilter('StringTrim')
+            ->addFilter('StringToUpper')
+            ->addValidator(new Braintacle_Validate_ProductKey);
+        $this->addElement($key);
+
+        $submit = new Zend_Form_Element_Submit('submit');
+        $submit->setLabel('OK');
+        $this->addElement($submit);
+    }
+
+}

application/models/Database.php
@@ -74,6 +74,21 @@ class Model_Database
     }
 
     /**
+     * Return status of manual Windows product key support
+     *
+     * The ability to manually enter a Windows product key that differs from the
+     * detected one requires a table which is not part of an original OCS
+     * Inventory database. To support these databases, the presence of the table
+     * should be queried via this method and the table should be used only if
+     * available.
+     * @return bool TRUE if table exists
+     */
+    public static function supportsManualProductKey()
+    {
+        return in_array('braintacle_windows', self::_listTables());
+    }
+
+    /**
      * Return status of MSofficeKey plugin support
      *
      * The {@link http://wiki.ocsinventory-ng.org/index.php/Plugins:MSofficeKey

application/models/Windows.php
@@ -23,15 +23,21 @@
  * @package Models
  */
 /**
+ * Includes
+ */
+require_once ('Braintacle/Validate/ProductKey.php');
+/**
  * Windows-specific information for a computer
  *
  * Properties:
  *
+ * - <b>ComputerId:</b> ID of computer this instance belongs to
  * - <b>UserDomain:</b> Domain of logged in user (for local accounts this is identical to the computer name)
  * - <b>Company:</b> Company name (typed in at installation)
  * - <b>Owner:</b> Owner (typed in at installation)
  * - <b>ProductKey:</b> Product Key (aka license key, typed in at installation)
  * - <b>ProductId:</b> Product ID (installation-specific, may or may not be unique)
+ * - <b>ManualProductKey:</b> Manually overridden product key (entered in Braintacle console)
  * @package Models
  */
 class Model_Windows extends Model_Abstract
@@ -40,12 +46,49 @@ class Model_Windows extends Model_Abstract
     /** {@inheritdoc} */
     protected $_propertyMap = array(
         // Values from 'hardware' table
+        'ComputerId' => 'id',
         'UserDomain' => 'userdomain',
         'Company' => 'wincompany',
         'Owner' => 'winowner',
         'ProductKey' => 'winprodkey',
         'ProductId' => 'winprodid',
+        // Values from 'braintacle_windows' table
+        'ManualProductKey' => 'manual_product_key',
     );
+
+    /** {@inheritdoc} */
+    public function setProperty($property, $value)
+    {
+        if ($property == 'ManualProductKey') {
+            // Validate and store value in database
+            if (empty($value) or $value == $this->getProductKey()) {
+                $value = null;
+            } else {
+                $validator = new Braintacle_Validate_ProductKey;
+                if (!$validator->isValid($value)) {
+                    throw new UnexpectedValueException(array_pop($validator->getMessages()));
+                }
+            }
+
+            $db = Model_Database::getAdapter();
+            // A record might not exist yet, so try UPDATE first, then INSERT
+            if (!$db->update(
+                'braintacle_windows',
+                array('manual_product_key' => $value),
+                array('hardware_id = ?' => $this->getComputerId())
+            )) {
+                $db->insert(
+                    'braintacle_windows',
+                    array(
+                        'hardware_id' => $this->getComputerId(),
+                        'manual_product_key' => $value,
+                    )
+                );
+            }
+        }
+
+        parent::setProperty($property, $value);
+    }
 
     /**
      * Create Model_Windows object for given computer
@@ -58,12 +101,18 @@ class Model_Windows extends Model_Abstract
      **/
     public static function getWindows($computer)
     {
-        $windows = Model_Database::getAdapter()
-                   ->select()
-                   ->from('hardware', array('userdomain, wincompany, winowner, winprodkey, winprodid'))
-                   ->where('id = ?', $computer->getId())
-                   ->query()
-                   ->fetchObject(__CLASS__);
+        $select = Model_Database::getAdapter()
+                  ->select()
+                  ->from('hardware', array('id, userdomain, wincompany, winowner, winprodkey, winprodid'))
+                  ->where('id = ?', $computer->getId());
+        if (Model_Database::supportsManualProductKey()) {
+            $select->joinLeft(
+                'braintacle_windows',
+                'hardware.id = braintacle_windows.hardware_id',
+                array('manual_product_key')
+            );
+        }
+        $windows = $select->query()->fetchObject(__CLASS__);
         if (!$windows) {
             throw new UnexpectedValueException('Invalid computer ID: ' . $computer->getId());
         }

application/views/scripts/computer/windows.phtml
@@ -23,7 +23,7 @@
  */
 
 $computer = $this->computer;
-$windows = $computer->getWindows();
+$windows = $this->windows;
 
 print $this->inventoryHeader($computer);
 
@@ -49,15 +49,6 @@ print $this->htmlTag(
 
 print $this->htmlTag(
     'dt',
-    $this->translate('Product key')
-);
-print $this->htmlTag(
-    'dd',
-    $this->escape($windows->getProductKey())
-);
-
-print $this->htmlTag(
-    'dt',
     $this->translate('Product ID')
 );
 print $this->htmlTag(
@@ -65,4 +56,18 @@ print $this->htmlTag(
     $this->escape($windows->getProductId())
 );
 
+print $this->htmlTag(
+    'dt',
+    $this->translate('Product key (reported by agent)')
+);
+print $this->htmlTag(
+    'dd',
+    $this->escape($windows->getProductKey())
+);
+
 print "</dl>\n";
+
+// Print product key form if supported.
+if (isset($this->form)) {
+    print $this->form;
+}

doc/index.html
@@ -92,6 +92,11 @@ Software dictionaries are much easier to manage.
 Extra categories are not supported yet &ndash; The dictionary manager is intended for software filters only.
 </li>
 <li>
+In addition to the automatically collected Windows license key, a key can be entered manually.
+This can be useful if the reported key is garbage (this can happen in certain situations)
+or if licenses have been reassigned within an organization.
+</li>
+<li>
 Consistent navigation &ndash; no popups, no more lost menus.
 <li>
 Computers can be exported as XML, either individually via console or all at once via the <a href="#export.php">export tool</a>.

languages/braintacle-library.pot
@@ -42,6 +42,10 @@ msgstr ""
 msgid "Only date can be given, not time of day"
 msgstr ""
 
+#: Validate/ProductKey:41
+msgid "'%value%' is not a valid product key."
+msgstr ""
+
 #: Validate/URI.php:23
 msgid "'%value%' is not a valid URI (without '%scheme%://')"
 msgstr ""

commit 76d16791df63dcb45a3c7c2bb702261a7eac42a6

Fixed abort when populating an empty database (regression from 6bfb1b016bc9f73823ea4bfb4f90eede96020c98)

library/Braintacle/SchemaManager.php
@@ -293,9 +293,16 @@ class Braintacle_SchemaManager
         if (!$dir) {
             throw new RuntimeException('Can\'t get a handle for schema directory');
         }
+        // Process this file first because other table definitions depend on it
+        $files = array('ocs_hardware.xml');
         while (($file = readdir($dir)) !== false ) {
-            if (substr($file, -4) != '.xml')
-                continue; // Ignore files without .xml suffix
+            if (substr($file, -4) == '.xml' and // Ignore files without .xml suffix
+                !in_array($file, $files) // Not already in list
+            ) {
+                $files[] = $file;
+            }
+        }
+        foreach ($files as $file) {
             $content = file_get_contents("$this->_basepath/schema/$file");
             if ($content == false) {
                 throw new RuntimeException("Error reading $file");
@@ -374,6 +381,7 @@ class Braintacle_SchemaManager
             'accountinfo',
             'accountinfo_config',
             'bios',
+            'braintacle_windows',
             'controllers',
             'devices',
             'download_available',