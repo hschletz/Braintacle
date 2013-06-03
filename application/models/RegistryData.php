7acb5a2c99255a131440d651a7c79766b7f3d992
Holger Schletz <holger.schletz@web.de>
2013-06-03 19:26:27 +0200

    Retrieve full Model_RegistryValue object instead of just its name.

--- application/models/RegistryData.php
+++ application/models/RegistryData.php
@@ -25,7 +25,7 @@
  *
  * Properties:
  *
- * - **Value** Name under which the value is stored in the registry configuration
+ * - **Value** Model_RegistryValue object
  * - **Data** Registry Data
  *
  * Don't confuse 'Value' with its content. In registry terms, 'Value' refers to
@@ -39,14 +39,77 @@ class Model_RegistryData extends Model_ChildObject
 {
     /** {@inheritdoc} */
     protected $_propertyMap = array(
-        // Values from 'registry' table
-        'Value' => 'name',
-        'Data' => 'regvalue',
+        'Value' => '', // populated manually
+        'Data' => 'regvalue', // from 'registry' table
+    );
+
+    /** {@inheritdoc} */
+    protected $_types = array(
+        'Value' => 'Model_RegistryValue',
     );
 
     /** {@inheritdoc} */
     protected $_tableName = 'registry';
 
     /** {@inheritdoc} */
-    protected $_preferredOrder = 'Value';
+    protected $_preferredOrder = 'Value'; // createStatement() turns this into the 'Name' property
+
+    /** {@inheritdoc} */
+    public function createStatement(
+        $columns=null,
+        $order=null,
+        $direction='asc',
+        $filters=null,
+        $query=true
+    )
+    {
+        // Call parent implementation without querying
+        $select = parent::createStatement(
+            $columns,
+            $order,
+            $direction,
+            $filters,
+            false
+        );
+        // Join regconfig table, rename ambiguous 'regvalue' column
+        $select->join(
+            'regconfig',
+            'regconfig.name = registry.name',
+            array('id', 'name', 'regtree', 'regkey', 'value' => 'regvalue')
+        );
+        if ($order == 'Value') {
+            // Order is invalid. Replace with name.
+            $select->reset('order');
+            $select->order("regconfig.name $direction");
+        }
+
+        if ($query) {
+            return $select->query();
+        } else {
+            return $select;
+        }
+    }
+
+    /** {@inheritdoc} */
+    function __set($property, $value)
+    {
+        if (in_array($property, $this->_propertyMap)) {
+            parent::__set($property, $value);
+        } else {
+            // Unknown columns are passed to Model_RegistryValue. If an instance
+            // does not exist yet, an exception is thrown which must be caught
+            // to create and store that instance.
+            try {
+                $regValue = $this->getValue();
+            } catch (Exception $e) {
+                $regValue = new Model_RegistryValue;
+                $this->setValue($regValue);
+            }
+            // Undo temporary renaming of ambiguous column
+            if ($property == 'value') {
+                $property = 'regvalue';
+            }
+            $regValue->$property = $value;
+        }
+    }
 }
