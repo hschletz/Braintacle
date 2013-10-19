196846dcb7510c3ee698df21247430feb714a08c
Holger Schletz <holger.schletz@web.de>
2013-10-19 11:42:29 +0200

    New version of the table view helper.

---
+++ module/Console/Console/Test/View/Helper/TableTest.php
@@ -0,0 +1,264 @@
+<?php
+/**
+ * Tests for the Table helper
+ *
+ * Copyright (C) 2011-2013 Holger Schletz <holger.schletz@web.de>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+namespace Console\Test\View\Helper;
+
+/**
+ * Tests for the Table helper
+ */
+class TableTest extends \Library\Test\View\Helper\AbstractTest
+{
+    /**
+     * Sample header data
+     * @var array
+     */
+    protected $_headers = array(
+        'column1' => 'header1',
+        'column2' => 'header2',
+    );
+
+    /**
+     * Collection of data rendered by renderCallback()
+     * @var array
+     */
+    protected $_renderCallbackData;
+
+    /**
+     * Mock for row()
+     *
+     * This is a simplified row renderer. Columns are separated by "|". Rows are
+     * terminated by "\n". Headers are converted uppercase.
+     *
+     * @param string[] $columns
+     * @param bool $isHeader
+     * @return string
+     */
+    public function mockRow(array $columns, $isHeader)
+    {
+        if ($isHeader) {
+            $columns = array_map('strtoupper', $columns);
+        }
+        return implode('|', $columns) . "\n";
+    }
+
+    /**
+     * Sample callback for cell rendering
+     *
+     * Cell data is returned unchanged, but also appended to
+     * $_renderCallbackData which can be evaluated after the table is rendered.
+     *
+     * @param \Zend\View\Renderer\RendererInterface $view
+     * @param array $rowData
+     * @param string $key
+     * @return mixed
+     */
+    public function renderCallback(\Zend\View\Renderer\RendererInterface $view, array $rowData, $key)
+    {
+        $this->_renderCallbackData[] = $rowData[$key];
+        return $rowData[$key];
+    }
+
+    /**
+     * Tests for the __invoke() method
+     */
+    public function testInvoke()
+    {
+        // Sample table data
+        $data = array(
+            array(
+                'column1' => 'value1a',
+                'column2' => 'value2a',
+                'unused' => 'value', // Extra column, should not be evaluated
+            ),
+            array(
+                // Test different order
+                'column2' => 'value2b',
+                'column1' => 'value1b',
+            ),
+        );
+        // Expected result when combined with $_headers
+        $expectedTable = "<table class='alternating'>\nHEADER1|HEADER2\nvalue1a|value2a\nvalue1b|value2b\n</table>\n";
+
+        // Stubs for row() and sortableHeader()
+        $rowCallback = array($this, 'mockRow');
+        $stubs = array('sortableHeader', 'row');
+
+        // Basic test with minimal arguments
+        $escapeHtml = $this->getMock('Zend\View\Helper\EscapeHtml');
+        $escapeHtml->expects($this->exactly(4)) // once per non-header cell
+                   ->method('__invoke')
+                   ->will($this->returnArgument(0));
+
+        $table = $this->getMock($this->_getHelperClass(), $stubs);
+        $table->expects($this->never()) // No sortable headers in this test
+              ->method('sortableHeader');
+        $table->expects($this->exactly(3))
+              ->method('row')
+              ->will($this->returnCallback($rowCallback));
+
+        $helper = $this->_getHelper(
+            array(
+                'Table' => $table,
+                'EscapeHtml' => $escapeHtml,
+            )
+        );
+        $this->assertEquals($expectedTable, $helper($data, $this->_headers));
+
+        // Test with sortable headers
+        $escapeHtml = $this->getMock('Zend\View\Helper\EscapeHtml');
+        $escapeHtml->expects($this->exactly(4)) // once per non-header cell
+                   ->method('__invoke')
+                   ->will($this->returnArgument(0));
+
+        $table = $this->getMock($this->_getHelperClass(), $stubs);
+        $table->expects($this->exactly(2)) // once per column
+              ->method('sortableHeader')
+              ->will($this->returnArgument(0));
+        $table->expects($this->exactly(3))
+              ->method('row')
+              ->will($this->returnCallback($rowCallback));
+
+        $helper = $this->_getHelper(
+            array(
+                'Table' => $table,
+                'EscapeHtml' => $escapeHtml,
+            )
+        );
+        $this->assertEquals(
+            $expectedTable,
+            $helper(
+                $data,
+                $this->_headers,
+                array('order' => 'column1', 'direction' => 'asc')
+            )
+        );
+
+        // Test with render callback on column2
+        $escapeHtml = $this->getMock('Zend\View\Helper\EscapeHtml');
+        $escapeHtml->expects($this->exactly(2)) // once per non-header cell that is not rendered via callback
+                   ->method('__invoke')
+                   ->will($this->returnArgument(0));
+
+        $table = $this->getMock($this->_getHelperClass(), $stubs);
+        $table->expects($this->never()) // No sortable headers in this test
+              ->method('sortableHeader');
+        $table->expects($this->exactly(3))
+              ->method('row')
+              ->will($this->returnCallback($rowCallback));
+
+        $helper = $this->_getHelper(
+            array(
+                'Table' => $table,
+                'EscapeHtml' => $escapeHtml,
+            )
+        );
+        $this->_renderCallbackData = array();
+        $this->assertEquals(
+            $expectedTable,
+            $helper(
+                $data,
+                $this->_headers,
+                array(),
+                array('column2' => array($this, 'renderCallback'))
+            )
+        );
+        $this->assertEquals(array('value2a', 'value2b'), $this->_renderCallbackData);
+    }
+
+    /**
+     * Tests for the sortableHeader() method
+     */
+    public function testSortableHeader()
+    {
+        // Test for current column sorted ascending: Arrow indicator up, link sorts descending
+        $consoleUrl = $this->getMock('Console\View\Helper\ConsoleUrl');
+        $consoleUrl->expects($this->once())
+                   ->method('__invoke')
+                   ->with(null, null, array('order' => 'Key', 'direction' => 'desc'), true)
+                   ->will($this->returnValue('ConsoleUrlMock'));
+        $htmlTag = $this->getMock('Library\View\Helper\HtmlTag');
+        $htmlTag->expects($this->once())
+                ->method('__invoke')
+                ->with('a', 'Label&uarr;', array('href' => 'ConsoleUrlMock'))
+                ->will($this->returnValue('HtmlTagMock'));
+        $helper = $this->_getHelper(
+            array(
+                'ConsoleUrl' => $consoleUrl,
+                'HtmlTag' => $htmlTag,
+            )
+        );
+        $this->assertEquals('HtmlTagMock', $helper->sortableHeader('Label', 'Key', 'Key', 'asc'));
+
+        // Test for current column sorted descending: Arrow indicator down, link sorts ascending
+        $consoleUrl = $this->getMock('Console\View\Helper\ConsoleUrl');
+        $consoleUrl->expects($this->once())
+                   ->method('__invoke')
+                   ->with(null, null, array('order' => 'Key', 'direction' => 'asc'), true)
+                   ->will($this->returnValue('ConsoleUrlMock'));
+        $htmlTag = $this->getMock('Library\View\Helper\HtmlTag');
+        $htmlTag->expects($this->once())
+                ->method('__invoke')
+                ->with('a', 'Label&darr;', array('href' => 'ConsoleUrlMock'))
+                ->will($this->returnValue('HtmlTagMock'));
+        $helper = $this->_getHelper(
+            array(
+                'ConsoleUrl' => $consoleUrl,
+                'HtmlTag' => $htmlTag,
+            )
+        );
+        $this->assertEquals('HtmlTagMock', $helper->sortableHeader('Label', 'Key', 'Key', 'desc'));
+
+        // Test for current column not sorted: No arrow indicator, link sorts ascending
+        $consoleUrl = $this->getMock('Console\View\Helper\ConsoleUrl');
+        $consoleUrl->expects($this->once())
+                   ->method('__invoke')
+                   ->with(null, null, array('order' => 'Key', 'direction' => 'asc'), true)
+                   ->will($this->returnValue('ConsoleUrlMock'));
+        $htmlTag = $this->getMock('Library\View\Helper\HtmlTag');
+        $htmlTag->expects($this->once())
+                ->method('__invoke')
+                ->with('a', 'Label', array('href' => 'ConsoleUrlMock'))
+                ->will($this->returnValue('HtmlTagMock'));
+        $helper = $this->_getHelper(
+            array(
+                'ConsoleUrl' => $consoleUrl,
+                'HtmlTag' => $htmlTag,
+            )
+        );
+        $this->assertEquals('HtmlTagMock', $helper->sortableHeader('Label', 'Key', 'Order', 'desc'));
+    }
+
+    /**
+     * Tests for the row() method
+     */
+    public function testRow()
+    {
+        $helper = $this->_getHelper();
+        $this->assertEquals(
+            "<tr>\n<td>\nheader1\n</td>\n<td>\nheader2\n</td>\n\n</tr>\n",
+            $helper->row($this->_headers, false)
+        );
+        $this->assertEquals(
+            "<tr>\n<th>\nheader1\n</th>\n<th>\nheader2\n</th>\n\n</tr>\n",
+            $helper->row($this->_headers, true)
+        );
+    }
+}
---
+++ module/Console/Console/View/Helper/Table.php
@@ -0,0 +1,154 @@
+<?php
+/**
+ * Generate HTML table
+ *
+ * Copyright (C) 2011-2013 Holger Schletz <holger.schletz@web.de>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+namespace Console\View\Helper;
+
+/**
+ * Generate HTML table
+ */
+class Table extends \Zend\View\Helper\AbstractHelper
+{
+/**
+ * Generate HTML table
+ *
+ * $headers is an associative array with header labels. Its keys are used to
+ * match corresponding fields in the other arguments. For each header, a
+ * corresponding field must be set in the table data or in $renderCallbacks.
+ *
+ * $data is an array of row objects. Row objects are associative arrays or
+ * objects implementing the \ArrayAccess interface.
+ *
+ * By default, cell data is retrieved from $data and escaped automatically.
+ * Alternatively, a callback can be provided in the $renderCallbacks array.
+ * If a callback is defined for a column, the callback is responsible for
+ * escaping cell data. It gets called with the following arguments:
+ *
+ * 1. The view renderer
+ * 2. The row object
+ * 3. The key of the column to be rendered. This is useful for callbacks that
+ *    render more than 1 column.
+ *
+ * If the optional $sorting array contains the "order" and "direction" elements
+ * (other elements are ignored), headers are generated as hyperlinks with
+ * "order" and "direction" parameters set to the corresponding column. The
+ * values denote the sorting in effect for the current request - the header will
+ * be marked with an arrow indicating the current sorting. The controller action
+ * should evaluate these parameters, sort the data and provide the sorting to
+ * the view renderer. The \Console\Mvc\Controller\Plugin\GetOrder controller
+ * plugin simplifies these tasks.
+ *
+ * @param array $data
+ * @param array $headers
+ * @param array $sorting
+ * @param array $renderCallbacks
+ */
+    function __invoke(
+        array $data,
+        array $headers,
+        $sorting=array(),
+        $renderCallbacks=array()
+    )
+    {
+        $table = "<table class='alternating'>\n";
+
+        // Generate header row
+        if (isset($sorting['order']) and isset($sorting['direction'])) {
+            $row = array();
+            foreach ($headers as $key => $label) {
+                $row[] = $this->sortableHeader($label, $key, $sorting['order'], $sorting['direction']);
+            }
+            $table .= $this->row($row, true);
+        } else {
+            $table .= $this->row($headers, true);
+        }
+
+        // Generate data rows
+        $keys = array_keys($headers);
+        foreach ($data as $rowData) {
+            $row = array();
+            foreach ($keys as $key) {
+                if (isset($renderCallbacks[$key])) {
+                    $row[] = $renderCallbacks[$key]($this->view, $rowData, $key);
+                } else {
+                    $row[] = $this->view->escapeHtml($rowData[$key]);
+                }
+            }
+            $table .= $this->row($row, false);
+        }
+
+        $table .= "</table>\n";
+        return $table;
+    }
+
+    /**
+     * Generate a header hyperlink
+     *
+     * @param string $label Header text. An arrow will be added to the currently sorted column.
+     * @param string $key Sort key to be used in the URL
+     * @param string $order Current order
+     * @param string $direction Current direction
+     * @return string HTML Hyperlink
+     */
+    public function sortableHeader($label, $key, $order, $direction)
+    {
+        if ($key == $order) {
+            // add arrow indicator to currently sorted column and
+            // invert direction for the hyperlink.
+            if ($direction == 'asc') {
+                $linkDirection = 'desc';
+                $label .= '&uarr;';
+            } else {
+                $linkDirection = 'asc';
+                $label .= '&darr;';
+            }
+        } else {
+            // use ascending ordering for every other hyperlink.
+            $linkDirection = 'asc';
+        }
+
+        $params = array(
+            'order' => $key,
+            'direction' => $linkDirection
+        );
+        return $this->view->htmlTag(
+            'a',
+            $label,
+            array('href' => $this->view->consoleUrl(null, null, $params, true)),
+            true
+        );
+    }
+
+    /**
+     * Generate a table row
+     * 
+     * @param array $columns Column data
+     * @param bool $isHeader Use "th" tag instead of "td".
+     * @return string HTML table row
+     */
+    public function row(array $columns, $isHeader)
+    {
+        $row = '';
+        foreach ($columns as $column) {
+            $row .= $this->view->htmlTag($isHeader ? 'th' : 'td', $column);
+        }
+        return $this->view->htmlTag('tr', $row);
+    }
+}
