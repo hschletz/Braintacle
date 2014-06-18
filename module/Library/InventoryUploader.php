17daf6517aed680c44cb92a85536aa426cbf677f
Holger Schletz <holger.schletz@web.de>
2014-06-18 11:19:13 +0200

    New inventory uploader class.

---
+++ module/Library/InventoryUploader.php
@@ -0,0 +1,85 @@
+<?php
+/**
+ * Upload an inventory file to the communication server
+ *
+ * Copyright (C) 2011-2014 Holger Schletz <holger.schletz@web.de>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+namespace Library;
+
+/**
+ * Upload an inventory file to the communication server
+ */
+class InventoryUploader
+{
+    /**
+     * Application config
+     * @var \Model\Config
+     */
+    protected $_config;
+
+    /**
+     * HTTP adapter
+     * @var string|\Zend\Http\Client\Adapter\AdapterInterface
+     */
+    protected $_adapter;
+
+    /**
+     * Constructor
+     *
+     * @param \Model\Config $config
+     * @param string|\Zend\Http\Client\Adapter\AdapterInterface $adapter default: 'Zend\Http\Client\Adapter\Socket'
+     */
+    public function __construct(\Model\Config $config, $adapter = 'Zend\Http\Client\Adapter\Socket')
+    {
+        $this->_config = $config;
+        $this->_adapter = $adapter;
+    }
+
+    /**
+     * Upload a file
+     *
+     * @param string $fileName File name
+     * @return \Zend\Http\Response Server response
+     */
+    public function uploadFile($fileName)
+    {
+        return $this->uploadData(\Library\FileObject::fileGetContents($fileName));
+    }
+
+    /**
+     * Upload inventory data
+     *
+     * @param string $data Inventory data (XML or zlib-compressed XML)
+     * @return \Zend\Http\Response Server response
+     */
+    public function uploadData($data)
+    {
+        $client = new \Zend\Http\Client(
+            $this->_config->communicationServerUri,
+            array(
+                'adapter' => $this->_adapter,
+                'strictredirects' => true, // required for POST requests
+                'useragent' => 'Braintacle_local_upload', // Substring 'local' required for correct server operation
+            )
+        );
+        $client->setMethod('POST')
+               ->setHeaders(array('Content-Type' => 'application/x-compress'))
+               ->setRawBody($data);
+        return $client->send();
+    }
+}
---
+++ module/Library/Test/InventoryUploaderTest.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * Tests for the InventoryUploader class
+ *
+ * Copyright (C) 2011-2014 Holger Schletz <holger.schletz@web.de>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+namespace Library\Test;
+
+use \Library\InventoryUploader;
+use \org\bovigo\vfs\vfsStream;
+
+/**
+ * Tests for the InventoryUploader class
+ */
+class InventoryUploaderTest extends \PHPUnit_Framework_TestCase
+{
+    public function testUploadFile()
+    {
+        $content = "testUploadFile\nline1\nline2\n";
+        $root = vfsstream::setup('root');
+        $url = vfsStream::newFile('test.txt')->withContent($content)->at($root)->url();
+        $uploader = $this->getMockBuilder('Library\InventoryUploader')
+                         ->disableOriginalConstructor()
+                         ->setMethods(array('uploadData'))
+                         ->getMock();
+        $uploader->expects($this->once())
+                 ->method('uploadData')
+                 ->with($content)
+                 ->will($this->returnValue('response'));
+        $this->assertEquals('response', $uploader->uploadFile($url));
+    }
+
+    public function testUploadData()
+    {
+        $config = $this->getMockBuilder('Model\Config')->disableOriginalConstructor()->getMock();
+        $config->expects($this->any())
+               ->method('__get')
+               ->with('communicationServerUri')
+               ->will($this->returnValue('http://example.net/server'));
+        $content = "testUploadFile\nline1\nline2\n";
+        $adapter = $this->getMockBuilder('Zend\Http\Client\Adapter\Test')->setMethods(array('write'))->getMock();
+        $adapter->expects($this->once())
+                ->method('write')
+                ->with(
+                    'POST',
+                    'http://example.net/server',
+                    '1.1',
+                    $this->callback(
+                        function($headers) {
+                            return (
+                                $headers['User-Agent'] == 'Braintacle_local_upload' and
+                                $headers['Content-Type'] == 'application/x-compress'
+                            );
+                        }
+                    ),
+                    $content
+                );
+        $uploader = new InventoryUploader($config, $adapter);
+        $uploader->uploadData($content);
+
+        // No public method available to test "strictredirects" option - use reflection
+        $reflectionClass = new \ReflectionClass($adapter);
+        $reflectionProperty = $reflectionClass->getProperty('config');
+        $reflectionProperty->setAccessible(true);
+        $this->assertTrue($reflectionProperty->getValue($adapter)['strictredirects']);
+    }
+}
