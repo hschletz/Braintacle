919c54f512b43e1c12481e97214b4174948bf86f
Holger Schletz <holger.schletz@web.de>
2015-05-30 12:56:33 +0200

    Fixed namespace.

--- module/Protocol/Test/Hydrator/DatabaseProxyTest.php
+++ module/Protocol/Test/Hydrator/DatabaseProxyTest.php
@@ -19,7 +19,7 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-namespace Protocol\Hydrator;
+namespace Protocol\Test\Hydrator;
 
 class DatabaseProxyTest extends \PHPUnit_Framework_TestCase
 {
