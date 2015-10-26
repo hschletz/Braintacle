9dae4846b8418dca98e7a24d384ddecadb43f141
Holger Schletz <holger.schletz@web.de>
2015-10-26 10:39:21 +0100

    Migrated Attachments table.

---
+++ module/Database/Table/Attachments.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * "temp_files" table
+ *
+ * Copyright (C) 2011-2015 Holger Schletz <holger.schletz@web.de>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+Namespace Database\Table;
+
+/**
+ * "temp_files" table
+ */
+class Attachments extends \Database\AbstractTable
+{
+    /**
+     * Client attachment
+     */
+    const OBJECT_TYPE_CLIENT = 'accountinfo';
+
+    /**
+     * SNMP-scanned device attachment
+     */
+    const OBJECT_TYPE_SNMP = 'snmp_accountinfo';
+
+    /**
+     * {@inheritdoc}
+     * @codeCoverageIgnore
+     */
+    public function __construct(\Zend\ServiceManager\ServiceLocatorInterface $serviceLocator)
+    {
+        $this->table = 'temp_files';
+        parent::__construct($serviceLocator);
+    }
+
+    /**
+     * {@inheritdoc}
+     * @codeCoverageIgnore
+     */
+    protected function _preSetSchema($logger, $schema, $database)
+    {
+        if (in_array($this->table, $database->getTableNames())) {
+            $count = $this->delete(
+                array(
+                    new \Zend\Db\Sql\Predicate\Operator('table_name', '!=', self::OBJECT_TYPE_CLIENT),
+                    new \Zend\Db\Sql\Predicate\Operator('table_name', '!=', self::OBJECT_TYPE_SNMP),
+                )
+            );
+            if ($count) {
+                $logger->info("Deleted $count unsupported attachments.");
+            }
+        }
+    }
+}
