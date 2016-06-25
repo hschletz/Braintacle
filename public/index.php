10d98c8bc527436c57ee973921efadb16df57375
Holger Schletz <holger.schletz@web.de>
2016-06-25 14:09:40 +0200

    Removed error display configuration (should be handled by environment instead).

--- public/index.php
+++ public/index.php
@@ -19,18 +19,9 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-// Set up PHP environment. The error configuration is done as early as possible.
 error_reporting(-1);
 
 require_once('../module/Library/Application.php');
-
-if (\Library\Application::isProduction()) {
-    ini_set('display_errors', false);
-    ini_set('display_startup_errors', false);
-} else {
-    ini_set('display_errors', true);
-    ini_set('display_startup_errors', true);
-}
 
 \Library\Application::init(
     (getenv('BRAINTACLE_CONFIG') ?: (__DIR__ . '/../config/braintacle.ini')),
