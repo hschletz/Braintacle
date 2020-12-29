7b945abb0a190bfc2f70b9f3b445427988b4b21c
Holger Schletz <holger.schletz@web.de>
2020-12-29 12:19:08 +0100

    Switched to phpDocumentor 3 for API documentation.

--- module/Tools/Controller/Apidoc.php
+++ module/Tools/Controller/Apidoc.php
@@ -38,42 +38,9 @@ class Apidoc
      */
     public function __invoke(\ZF\Console\Route $route, \Zend\Console\Adapter\AdapterInterface $console)
     {
-        $console->writeLine('Running Doxygen to generate documentation for dependencies...');
-
-        // Delete tagfile if it exists. Otherwise it wouldn't be used reliably.
-        try {
-            unlink(\Library\Application::getPath('doxygen/dependencies.tag'));
-        } catch (\Exception $e) {
-            // No error if it doesn't exist
-        }
-        $process = new Process(['doxygen', \Library\Application::getPath('doxygen/dependencies.Doxyfile')]);
-        $process->run(); // Ignore warnings
-
-        $console->writeLine('Running Doxygen to generate Braintacle API documentation...');
-
-        $process = new Process(['doxygen', \Library\Application::getPath('doxygen/braintacle.Doxyfile')]);
+        $process = new Process(['tools/phpDocumentor'], \Library\Application::getPath());
         $process->run(function ($type, $buffer) use ($console) {
-            if (strpos($buffer, '<unknown>:1: warning: Detected potential recursive class relation') !== 0 and
-                strpos($buffer, 'warning: Internal inconsistency: scope for class') === false and
-                !preg_match('/^deprecated:\d+: warning: Illegal command \w+ as part of a <dt> tag/', $buffer)
-            ) {
-                $console->write($buffer);
-            }
+            $console->write($buffer);
         });
-
-        $console->writeLine('Postprocessing documentation...');
-
-        $ignoreMessage = 'WARNING: could not parse ' . \Library\Application::getPath('doc/api/dependencies');
-        $process = new Process([
-            (new \Symfony\Component\Process\PhpExecutableFinder)->find(),
-            \Library\Application::getPath('vendor/bin/doxygen-phpdoc-fixhtml.php'),
-            \Library\Application::getPath('doc/api')
-        ]);
-        $process->run(function ($type, $buffer) use ($console, $ignoreMessage) {
-            if (strpos($buffer, $ignoreMessage) !== 0) {
-                $console->write($buffer);
-            }
-        });
-        $console->writeLine('Done.');
     }
 }
