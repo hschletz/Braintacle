f58b37d6f8d3d62ccd0d88f375c45330a35e9d25
Holger Schletz <holger.schletz@web.de>
2025-01-01 18:25:23 +0100

    Update copyright.

--- module/Library/Test/View/Helper/HtmlElementTest.php
+++ module/Library/Test/View/Helper/HtmlElementTest.php
@@ -3,7 +3,7 @@
 /**
  * Tests for the HtmlElement helper
  *
- * Copyright (C) 2011-2024 Holger Schletz <holger.schletz@web.de>
+ * Copyright (C) 2011-2025 Holger Schletz <holger.schletz@web.de>
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the Free
