4741724020fba70dafb725efa8f4c6e212158650
Holger Schletz <holger.schletz@web.de>
2025-01-19 14:52:20 +0100

    Add virtual property descriptions to group model.

--- module/Model/Group/Group.php
+++ module/Model/Group/Group.php
@@ -43,6 +43,14 @@ use Random\Randomizer;
  * become a member by manual assignment or automatically based on the result of
  * a query. It is also possible to unconditionally exclude a client from a group
  * regardless of query result.
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $description
+ * @property DateTimeInterface $creationDate
+ * @property string $dynamicMembersSql SQL query for dynamic members, may be empty
+ * @property DateTimeInterface $cacheCreationDate Timestamp of last cache update
+ * @property DateTimeInterface $cacheExpirationDate Timestamp when cache will expire and get rebuilt
  */
 class Group extends \Model\ClientOrGroup
 {
