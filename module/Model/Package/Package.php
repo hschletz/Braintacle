71ef9db19a821d70b96643b93ef61647725598aa
Holger Schletz <holger.schletz@web.de>
2025-02-23 17:57:19 +0100

    Declare package properties lowercase.

--- module/Model/Package/Package.php
+++ module/Model/Package/Package.php
@@ -29,39 +29,39 @@ namespace Model\Package;
  * by PackageManager. Manipulation by any other means is not supported and can
  * give unpredictable results.
  *
- * @property integer $Id Numeric package ID, historically a UNIX timestamp
- * @property string $Name Name to uniquely identify package
- * @property \DateTime $Timestamp Timestamp of package creation
- * @property integer $Priority Download priority (0-10)
- * @property integer $NumFragments Number of download fragments
- * @property integer $Size Download size
- * @property string $Platform One of 'windows', 'linux' or 'mac'
- * @property string $Comment Comment
- * @property integer $NumPending Number of clients with pending packages,
+ * @property integer $id Numeric package ID, historically a UNIX timestamp
+ * @property string $name Name to uniquely identify package
+ * @property \DateTime $timestamp Timestamp of package creation
+ * @property integer $priority Download priority (0-10)
+ * @property integer $numFragments Number of download fragments
+ * @property integer $size Download size
+ * @property string $platform One of 'windows', 'linux' or 'mac'
+ * @property string $comment Comment
+ * @property integer $numPending Number of clients with pending packages,
  * provided by PackageManager::getPackages()
- * @property integer $NumRunning Number of clients currently downloading/installing package,
+ * @property integer $numRunning Number of clients currently downloading/installing package,
  * provided by PackageManager::getPackages()
- * @property integer $NumSuccess Number of clients with successful deployment,
+ * @property integer $numSuccess Number of clients with successful deployment,
  * provided by PackageManager::getPackages()
- * @property integer $NumError Number of clients with unsuccessful deployment,
+ * @property integer $numError Number of clients with unsuccessful deployment,
  * provided by PackageManager::getPackages()
- * @property string $HashType Hash type (recommended: SHA256 for Windows packages, SHA1 for others)
- * @property string $Hash Hash of assembled package,
- * @property string $DeployAction One of 'store', 'execute', 'launch',
+ * @property string $hashType Hash type (recommended: SHA256 for Windows packages, SHA1 for others)
+ * @property string $hash Hash of assembled package,
+ * @property string $deployAction One of 'store', 'execute', 'launch',
  * provided by PackageManager::getPackage()
- * @property string $ActionParam Path for storage or command to execute, depending on action,
+ * @property string $actionParam Path for storage or command to execute, depending on action,
  * provided by PackageManager::getPackage()
- * @property bool $Warn Whether the user should be notified before deployment,
+ * @property bool $warn Whether the user should be notified before deployment,
  * provided by PackageManager::getPackage()
- * @property string $WarnMessage Message to display before deployment,
+ * @property string $warnMessage Message to display before deployment,
  * provided by PackageManager::getPackage()
- * @property integer $WarnCountdown Timeout in seconds before deployment starts,
+ * @property integer $warnCountdown Timeout in seconds before deployment starts,
  * provided by PackageManager::getPackage()
- * @property bool $WarnAllowAbort Whether the user should be allowed to abort,
+ * @property bool $warnAllowAbort Whether the user should be allowed to abort,
  * provided by PackageManager::getPackage()
- * @property bool $WarnAllowDelay Whether the user should be allowed to delay,
+ * @property bool $warnAllowDelay Whether the user should be allowed to delay,
  * provided by PackageManager::getPackage()
- * @property string $PostInstMessage Message to display after deployment,
+ * @property string $postInstMessage Message to display after deployment,
  * provided by PackageManager::getPackage()
  */
 class Package extends \Model\AbstractModel
